6e83d6c3942acb241e060ccf75d1bc934bf35866
Eduardo Couto <eduardo.lcouto@gmail.com>
2017-01-18 17:08:16 -0200

    refatorando materializecss

--- src/bill-pay/bill-pay-create.component.js
+++ src/bill-pay/bill-pay-create.component.js
@@ -1,22 +1,41 @@
 window.billPayCreateComponent = Vue.extend({
   template:`
-      <h3>Criar Conta</h3>
+<div class="container">
+      <h4 v-if="formType == 'insert'">Criar Conta</h4>
+      <h4 v-else>Editar Conta</h4>
       <form name="form" @submit.prevent="submit">
-        <div>
-          <label for="vencimento">Vencimento:</label>
-            <input name="vencimento" type="text" v-model="bill.date_due | formatDate">
-          <label for="names">Nome</label>
-            <select name="nomes" v-model="bill.name | formatString">
+        <div class="row">
+          <div class="input-field col s6">
+            <label for="vencimento" class="active">Vencimento</label>
+            <input id="vencimento" name="vencimento" type="text" v-model="bill.date_due | formatDate">
+          </div>
+          <div class="input-field col s6">
+          <label for="valor" class="active">Valor</label>
+            <input id="valor" name="valor" type="text" v-model="bill.value | formatNumber">
+          </div>
+        </div><!-- row -->
+        
+        <div class="row">
+          <div class="col s6">
+            <label for="nomes" class="active">Nome</label>
+            <select id="nomes" name="nomes" v-model="bill.name | formatString" class="browser-default">
+              <option value="0" disabled selected>Escolha um nome</option>
               <option v-for="o in names" value="{{o}}">{{o}}</option>
             </select>
-          <label for="valor">Valor</label>
-            <input name="valor" type="text" v-model="bill.value | formatNumber">
-          <label for="status">Paga:</label>
-          <input type="checkbox" v-model="bill.done">
-          <br>
-          <input type="submit" value="Enviar">
+          </div>
+          <div class="input-field col s6">
+            <input type="checkbox" v-model="bill.done" id="status">
+            <label for="status">Paga</label>
+          </div>
+        </div><!-- row -->
+        <div class="row">
+          <input v-if="formType == 'insert'" type="submit" value="Enviar" class="btn">
+          <input v-else type="submit" value="Atualizar" class="btn">
         </div>
       </form>
+</div>
+
+
   `,
   data(){
     return {
--- src/bill-pay/bill-pay.component.js
+++ src/bill-pay/bill-pay.component.js
@@ -2,34 +2,38 @@ window.billPayComponent = Vue.extend({
   components:{
     'menu-component': billPayMenuComponent,
   },
-  template: `<div class="container">
-                <h3>{{ title }}</h3>
-                <div class="row">
-                    <div class="col s6">
-                        <div class="card" :class="{'white black-text': status === false, 'green white-text': status === 0, 'red white-text ': status > 0}">
-                            <div class="card-content">
-                                <span class="card-title"><i class="material-icons">account_balance</i></span>
-                                <p >
-                                  {{ status | statusGeneral}}
-                                </p>
-                             </div>
-                         </div>
-                     </div>
+  template: `
+<div class="section">
+  <div class="container">
+      <h4>{{ title }}</h4>
+      <div class="row">
+          <div class="col s6">
+              <div class="card" :class="{'white black-text': status === false, 'green white-text': status === 0, 'red white-text ': status > 0}">
+                  <div class="card-content">
+                      <span class="card-title"><i class="material-icons">account_balance</i></span>
+                      <h5>
+                        {{ status | statusGeneral}}
+                      </h5>
+                   </div>
+               </div>
+           </div>
 
-                    <div class="col s6">
-                        <div class="card ">
-                            <div class="card-content">
-                            <span class="card-title"><i class="material-icons">payment</i></span>
-                                <p>
-                                    {{total | formatNumber}}
-                                 </p>
-                            </div>
-                        </div>
-                     </div>
-                </div>
-            </div>
-                <router-view></router-view>
-            `,
+          <div class="col s6">
+              <div class="card ">
+                  <div class="card-content">
+                  <span class="card-title"><i class="material-icons">payment</i></span>
+                      <h5>
+                          {{total | formatNumber}}
+                       </h5>
+                  </div>
+              </div>
+           </div>
+      </div>
+  </div>
+</div>
+<div class="divider"></div>
+    <router-view></router-view>
+`,
 
     data(){
         return {
